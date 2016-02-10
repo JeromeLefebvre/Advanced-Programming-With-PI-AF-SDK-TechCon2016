042e51c8b578ce50b3ce3d7b2c03083cc1284ff0
barry.shang <barry.shang@gmail.com>
2016-02-10 12:27:51 -0800

    Added async. In progress.

---
+++ Ex4-Asynchronous-Read-Write-Sln/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4_Asynchronous_Read_Write_Sln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
