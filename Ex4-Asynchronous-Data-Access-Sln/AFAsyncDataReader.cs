5dc40469d87efaccc1a03e5a8f5efc24a566067d
barry.shang <barry.shang@gmail.com>
2016-02-19 13:17:37 -0800

    Add missing file.

---
+++ Ex4-Asynchronous-Data-Access-Sln/AFAsyncDataReader.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using OSIsoft.AF.Asset;
+using OSIsoft.AF.Data;
+using OSIsoft.AF.Time;
+
+namespace Ex4_Asynchronous_Data_Access_Sln
+{
+    public class AFAsyncDataReader
+    {
+        public static async Task<IList<IDictionary<AFSummaryTypes, AFValue>>> GetSummariesAsync(AFAttributeList attributeList)
+        {
+            Console.WriteLine("Calling GetSummariesAsync");
+
+            AFSummaryTypes mySummaries = AFSummaryTypes.Minimum | AFSummaryTypes.Maximum | AFSummaryTypes.Average | AFSummaryTypes.Total;
+            AFTimeRange timeRange = new AFTimeRange(new AFTime("*-1d"), new AFTime("*"));
+
+            Task<IDictionary<AFSummaryTypes, AFValue>>[] tasks = attributeList
+                // Do not make the call if async is not supported
+                .Where(attr => (attr.SupportedDataMethods & AFDataMethods.Asynchronous) == AFDataMethods.Asynchronous)
+                .Select(async attr =>
+                {
+                    try
+                    {
+                        return await attr.Data.SummaryAsync(
+                            timeRange: timeRange,
+                            summaryType: mySummaries,
+                            calculationBasis: AFCalculationBasis.TimeWeighted,
+                            timeType: AFTimestampCalculation.Auto);
+                    }
+                    catch (AggregateException ae)
+                    {
+                        Console.WriteLine("{0}: {1}", attr.Name, ae.Flatten().InnerException.Message);
+                        return null;
+                    }
+                })
+                .ToArray();
+
+            return await Task.WhenAll(tasks);
+        }
+
+        public static async Task<IList<IDictionary<AFSummaryTypes, AFValue>>> GetSummariesAsyncThrottled(AFAttributeList attributeList)
+        {
+            // Use asynchronous semaphore pattern (e.g. SemaphoreSlim.WaitAsync()) to throttle the calls
+
+            Console.WriteLine("Calling GetSummariesAsyncThrottled");
+
+            AFSummaryTypes mySummaries = AFSummaryTypes.Minimum | AFSummaryTypes.Maximum | AFSummaryTypes.Average | AFSummaryTypes.Total;
+            AFTimeRange timeRange = new AFTimeRange(new AFTime("*-1d"), new AFTime("*"));
+
+            // Example: Limit to 20 concurrent async I/O operations.
+            SemaphoreSlim throttler = new SemaphoreSlim(initialCount: 20);
+
+            Task<IDictionary<AFSummaryTypes, AFValue>>[] tasks = attributeList
+                // Do not make the call if async is not supported
+                .Where(attr => (attr.SupportedDataMethods & AFDataMethods.Asynchronous) == AFDataMethods.Asynchronous)
+                .Select(async attr =>
+                {
+                    // asychronously try to acquire the semaphore
+                    await throttler.WaitAsync();
+
+                    try
+                    {
+                        return await attr.Data.SummaryAsync(
+                            timeRange: timeRange,
+                            summaryType: mySummaries,
+                            calculationBasis: AFCalculationBasis.TimeWeighted,
+                            timeType: AFTimestampCalculation.Auto);
+                    }
+                    catch (AggregateException ae)
+                    {
+                        Console.WriteLine("{0}: {1}", attr.Name, ae.Flatten().InnerException.Message);
+                        return null;
+                    }
+                    finally
+                    {
+                        // release the resource
+                        throttler.Release();
+                    }
+                })
+                .ToArray();
+
+            return await Task.WhenAll(tasks);
+        }
+
+        public static async Task<IList<IDictionary<AFSummaryTypes, AFValue>>> GetSummariesAsyncWithTimeout(AFAttributeList attributeList, int timeoutInMilliseconds)
+        {
+            // Use asynchronous semaphore pattern (e.g. SemaphoreSlim.WaitAsync()) to throttle the calls
+
+            Console.WriteLine("Calling GetSummariesAsyncWithTimeout");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+            CancellationTokenSource ctsForTimer = new CancellationTokenSource();
+            CancellationToken tokenForTimer = ctsForTimer.Token;
+
+            AFSummaryTypes mySummaries = AFSummaryTypes.Minimum | AFSummaryTypes.Maximum | AFSummaryTypes.Average | AFSummaryTypes.Total;
+            AFTimeRange timeRange = new AFTimeRange(new AFTime("*-1d"), new AFTime("*"));
+
+            Task<IDictionary<AFSummaryTypes, AFValue>>[] tasks = attributeList
+                // Do not make the call if async is not supported
+                .Where(attr => (attr.SupportedDataMethods & AFDataMethods.Asynchronous) == AFDataMethods.Asynchronous)
+                .Select(async attr =>
+                {
+                    try
+                    {
+                        return await attr.Data.SummaryAsync(
+                            timeRange: timeRange,
+                            summaryType: mySummaries,
+                            calculationBasis: AFCalculationBasis.TimeWeighted,
+                            timeType: AFTimestampCalculation.Auto,
+                            cancellationToken: token);
+                    }
+                    catch (AggregateException ae)
+                    {
+                        Console.WriteLine("{0}: {1}", attr.Element.Name, ae.Flatten().InnerException.Message);
+                        return null;
+                    }
+                    catch (OperationCanceledException oe)
+                    {
+                        Console.WriteLine("{0}: {1}", attr.Element.Name, oe.Message);
+                        return null;
+                    }
+                })
+                .ToArray();
+
+            // Define a task that completes when all subtasks are complete
+            Task<IDictionary<AFSummaryTypes, AFValue>[]> task = Task.WhenAll(tasks);
+
+            // Asychronously wait for either the summaries or timer task to complete
+            if (await Task.WhenAny(task, Task.Delay(timeoutInMilliseconds, tokenForTimer)) == task)
+            {
+                // Cancel the timer task
+                ctsForTimer.Cancel();
+                // Return summaries result
+                return task.Result;
+            }
+            else
+            {
+                // Cancel the summaries task if timeout
+                cts.Cancel();
+                throw new TimeoutException("The operation has timed out.");
+            }
+        }
+    }
+}
